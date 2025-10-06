3c2daeecf5b019fec2fcdf484ff1e58b266c4248
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-10-06 08:48:59 +0700

    Feat: add Logo component to display main app logo

---
+++ src/components/ui/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Logo = () => (
+  <div className="flex items-center justify-center">
+    <img 
+      src="/public/logo-coklat.svg" 
+      alt="Logo Coffee Shop" 
+      className="h-9 w-auto object-contain"
+    />
+  </div>
+);
+
+export default Logo;
