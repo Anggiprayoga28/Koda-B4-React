f46f47afa010ed947a44e5cf4622383aafab0c58
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-24 08:21:27 +0700

    Feat: Add Redux auth state logging to LandingPage

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,11 +5,17 @@ import MenuSection from '../components/landing/MenuSection';
 import MapSection from '../components/landing/MapSection';
 import TestimonialSection from '../components/landing/TestimonialSection';
 import WhatsAppFloating from '../components/ui/WhatsAppFloating';
+import { useSelector } from 'react-redux';
 
 const LandingPage = () => {
   useEffect(() => {
     document.title = 'Coffee Shop - Premium Coffee & Delicious Food';
   }, []);
+
+  const user = useSelector(state=> state.auth)
+  console.log(user)
+  const redux = useSelector(state=> state.auth)
+  console.log(redux)
 
   return (
     <div>
